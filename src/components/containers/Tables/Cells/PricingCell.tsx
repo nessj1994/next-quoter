81595bed14382a2610850809bca9e88b53d1f6de
Jordan <jness@litaniasports.com>
2022-01-25 12:09:13 -0600

    Added new functionality for pricing mode

--- src/components/containers/Tables/Cells/PricingCell.tsx
+++ src/components/containers/Tables/Cells/PricingCell.tsx
@@ -1,51 +1,125 @@
 import React, { useEffect } from 'react';
-import { CellProps } from 'react-table';
+import { CellProps, Row } from 'react-table';
+import {
+  useAppSelector,
+  editing,
+  useAppDispatch,
+  QuoteLine,
+} from '../../../../store';
+import { debounce } from 'utils';
 
-type IPricingCellProps = {
-  updateMyData: (props: any) => void;
+type PriceCellProps = CellProps<QuoteLine> & {
+  updateMyData;
 };
 
-type EditableCellProps = CellProps<any> & IPricingCellProps;
+function PricingCell({
+  value: initialValue,
+  column: { id, width },
+  row: { original, index, values },
+  updateMyData,
+}: PriceCellProps) {
+  // Array of the possible pricing modes
+  const pricingMethods = ['Default', 'Manual', 'Retail'];
+  // create a single source to reference debounce timer.
+  const debounceTimer = 500;
 
-function PricingCell(props: EditableCellProps) {
-  // We need to keep and update the state of the cell normally
-  const {
-    value: initialValue,
-    column: { id, width },
-    row: { index, values, original },
-    updateMyData,
-  } = props;
-  const [value, setValue] = React.useState(initialValue);
-
-  // console.log(props);
-
-  const onChange = (e: any) => {
-    setValue(e.target.value);
-    console.log(original);
-    updateMyData({ original, index, id, value: e.target.value });
+  const initPricing = () => {
+    return {
+      line_multiplier: original.line_multiplier,
+      manual_price: original.manual_price,
+      pricing_mode: original.pricing_mode,
+    };
   };
 
-  // We'll only update the external data when the input is blurred
-  const onBlur = () => {
-    updateMyData({ original, index, id, value });
-  };
+  const [value, setValue] = React.useState(initPricing);
 
-  // If the initialValue is changed external, sync it up with our state
-  React.useEffect(() => {
-    setValue(initialValue);
-  }, [initialValue]);
+  useEffect(() => {
+    let mounted = true;
+    if (mounted) {
+      setValue(initPricing);
+      console.log(value);
+    }
+    return () => {
+      mounted = false;
+    };
+  }, [original, initialValue]);
+
+  const onChangeMultiplier = debounce((e: any) => {
+    setValue({ ...value, line_multiplier: e.target.value });
+    updateMyData({
+      original,
+      index,
+      field: 'line_multiplier',
+      value: e.target.value,
+    });
+  }, debounceTimer);
+
+  const onChangeMode = debounce((e: any) => {
+    setValue({ ...value, pricing_mode: e.target.value });
+    updateMyData({
+      original,
+      index,
+      field: 'pricing_mode',
+      value: e.target.value,
+    });
+  }, debounceTimer);
+
+  const onChangeManualPrice = debounce((e: any) => {
+    setValue({ ...value, manual_price: e.target.value });
+    updateMyData({
+      original,
+      index,
+      field: 'manual_price',
+      value: e.target.value,
+    });
+  }, debounceTimer);
+
+  console.log(value);
+
+  // Destructure value for further ease. We opt not to do this above
+  // so that we can also access value by name
+
   return (
     <>
       <div className="container-fluid print:hidden">
-        <input
-          className="text-black custom-input w-100"
-          type="number"
-          value={value}
-          step={0.05}
-          onChange={onChange}
-          onBlur={onBlur}
-          // style={{ width: '50px' }}
-        />
+        <div>
+          <label htmlFor="price-method">Method</label>
+          <select
+            className="text-black custom-input w-100"
+            defaultValue={value.pricing_mode}
+            onChange={onChangeMode}
+          >
+            {pricingMethods.map((method, i) => (
+              <option value={i} key={`pricing-${method}`}>
+                {method}
+              </option>
+            ))}
+          </select>
+        </div>
+        {initialValue !== 1 && initialValue !== '1' ? (
+          <div className="container">
+            <label htmlFor="pricing">Multiplier</label>
+            <input
+              id="line-multiplier"
+              className="text-black custom-input w-100"
+              type="number"
+              defaultValue={value.line_multiplier}
+              step={0.05}
+              onChange={onChangeMultiplier}
+              // style={{ width: '50px' }}
+            />
+          </div>
+        ) : (
+          <div className="container">
+            <label htmlFor="pricing">Price</label>
+            <input
+              id="manual-price"
+              className="text-black custom-input w-100"
+              defaultValue={value.manual_price}
+              onChange={onChangeManualPrice}
+            />
+          </div>
+        )}
       </div>
     </>
   );
